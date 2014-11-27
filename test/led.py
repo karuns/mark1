de7e4914928c5f8e3f4eb1c3ebed207515b656ca
Sohan Karun <sohan.karun@gmail.com>
2014-11-26 16:28:31 -0800

    test

--- test/led.py
+++ test/led.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-pinNum = 4
+pinNum = 3
 GPIO.setmode(GPIO.BCM) #numbering scheme that corresponds to breakout board and pin layout
 GPIO.setup(pinNum,GPIO.OUT) #replace pinNum with whatever pin you used, this sets up that pin as an output
 #set LED to flash forever
