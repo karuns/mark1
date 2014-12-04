a33a658586ef2ed1ed4456071b50ac0beb5d2d10
Sohan Karun <sohan.karun@gmail.com>
2014-12-03 22:51:09 -0800

    adding axis 2 and 4 of joystick which is left and right joystick motion for both the stick

--- test/ps3_contoller_first.py
+++ test/ps3_contoller_first.py
@@ -18,34 +18,44 @@ print 'Initialized Joystick : %s' % j.get_name()
 # Setup the various GPIO values, using the BCM numbers for now
 MotorA0 = 16
 MotorA1 = 18
-MotorAE = 22
+MotorA00 = 6
+MotorA11 = 13
+
 
 MotorB0 = 23
 MotorB1 = 21
-MotorBE = 19
+MotorB00 = 11
+MotorB11 = 9
 
 A0 = False
 A1 = False
 B0 = False
 B1 = False
-
+A00 = False
+A11 = False
+B00 = False
+B11 = False
 
 
 GPIO.setup(MotorA0,GPIO.OUT)
 GPIO.setup(MotorA1,GPIO.OUT)
-GPIO.setup(MotorAE,GPIO.OUT)
+GPIO.setup(MotorA00,GPIO.OUT)
+GPIO.setup(MotorA11,GPIO.OUT)
 
 GPIO.setup(MotorB0,GPIO.OUT)
 GPIO.setup(MotorB1,GPIO.OUT)
-GPIO.setup(MotorBE,GPIO.OUT)
+GPIO.setup(MotorB00,GPIO.OUT)
+GPIO.setup(MotorB11,GPIO.OUT)
 
 # Set all the Motors to 'off'
 GPIO.output(MotorA0, A0)
 GPIO.output(MotorA1, A1)
-GPIO.output(MotorAE, False)
-GPIO.output(MotorBE, False)
 GPIO.output(MotorB0, B0)
 GPIO.output(MotorB1, B1)
+GPIO.output(MotorA00, A00)
+GPIO.output(MotorA11, A11)
+GPIO.output(MotorB00, B00)
+GPIO.output(MotorB11, B11)
 
 
 # Only start the motors when the inputs go above the following threshold
@@ -54,24 +64,23 @@ threshold = 0.60
 
 LeftTrack = 0
 RightTrack = 0
+LeftTurn = 0
+RightTurn = 0
 
 # Configure the motors to match the current settings.
 
 def setmotors():
         GPIO.output(MotorA0, A0)
         GPIO.output(MotorA1, A1)
-        GPIO.output(MotorAE, True)
-        GPIO.output(MotorBE, True)
         GPIO.output(MotorB0, B0)
         GPIO.output(MotorB1, B1)
-
+        GPIO.output(MotorA00, A00)
+        GPIO.output(MotorA11, A11)
+        GPIO.output(MotorB00, B00)
+        GPIO.output(MotorB11, B11)
 # Try and run the main code, and in case of failure we can stop the motors
 try:
-    # Turn on the motors
-    GPIO.output(MotorAE, True)
-    GPIO.output(MotorBE, True)
-
-    # This is the main loop
+    # Turn on the motors# This is the main loop
     while True:
 
         # Check for any queued events and then process each one
@@ -84,8 +93,14 @@ try:
             if event.axis == 1:
               LeftTrack = event.value
               UpdateMotors = 1
+            elif event.axis == 2:
+              LeftTurn = event.value
+              UpdateMotors = 1
             elif event.axis == 3:
               RightTrack = event.value
+              UpdateMotors = 1
+            elif event.axis == 4:
+              RightTurn = event.value
               UpdateMotors = 1
 
             # Check if we need to update what the motors are doing
@@ -106,6 +121,7 @@ try:
                   A0 = False
                   A1 = False
 
+
               # And do the same for the right motor
               if (LeftTrack > threshold):
                   B0 = False
@@ -119,15 +135,38 @@ try:
                   B0 = False
                   B1 = False
 
-              # Now we've worked out what is going on we can tell the
+              # Move LEft 
+              if (LeftTurn > threshold):
+                  B00 = False
+                  B11 = True
+              # Move backwards
+              elif (LeftTurn < -threshold):
+                  B00 = True
+                  B11 = False
+              # Stopping
+              else:
+                  B00 = False
+                  B11 = False
+                  
+                                
+                            # Move forwards
+              if (RightTurn > threshold):
+                  A00 = False
+                  A11 = True
+              # Move backwards
+              elif (RightTurn < -threshold):
+                  A00 = True
+                  A11 = False
+              # Stopping
+              else:
+                  A00 = False
+                  A11 = False                   # Now we've worked out what is going on we can tell the
               # motors what they need to do
               setmotors()
 
 
 except KeyboardInterrupt:
     # Turn off the motors
-    GPIO.output(MotorAE, False)
-    GPIO.output(MotorBE, False)
     j.quit()#!/usr/bin/env python
 
 GPIO.cleanup()
